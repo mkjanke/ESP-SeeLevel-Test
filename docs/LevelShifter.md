7a1cf2eab7afc8b9580558acce6b6836b1c559cd
Michael Janke <mkjanke@gmail.com>
2024-01-11 20:19:36 -0600

    Refactor. Separate read pin from write pin.

--- docs/LevelShifter.md
+++ docs/LevelShifter.md
@@ -1,41 +1,9 @@
-## Basic 3.3V <-> 12V level shifter
+## 3.3V <-> 12V SeeLevel Interface
 
-To interface, I built a level-shifter using this document as a guide:
-
-  https://www.analog.com/en/design-notes/how-to-level-shift-1wire-systems.html
-
-The 'Basic Level Shifter' was built with the right-hand side powered by a 12V power supply, an ESP32 as the 3.3V side, and a 2N7000 N-channel MOSFET. Resisters were chosen by guessing. I ended up with a 51kohm pullup resister on the 3.3V side and a 560ohm resister on the 12V side, which limits current to approx 25mA. I am not an EE.
-
-![My Image](SeeLevelSensorCircuit.png)
-
-Operation is described in the document linked below.
-
-https://www.analog.com/en/design-notes/how-to-level-shift-1wire-systems.html
-
-### Description of the Level-Shifting Operation
-
-> To understand how the logic shifter works for any open-drain bus line, there are three main states to consider during a bi-directional transmission.
-
-#### State 1 – No device pulls down the line to a low level
-
-> With nothing pulling down on the bus line, the lower voltage side gets pulled up to 3.3V by its pullup resistor. In this event, the MOSFET's VGS is below the threshold voltage because the gate and the source are both at 3.3V, so the MOSFET does not conduct. This allows the bus line at the higher voltage side to get pulled up to 5V (12V) by its pullup resistor. As a result, both sides are high but at different voltage levels. 
-
-[Note: Should be no current on ESP32 Pin]
-
-#### State 2 – 3.3V system pulls down the open-drain bus to a low level
-
-> With the lower voltage side pulling down on the bus line, the MOSFET's source gets pulled down low and leaves the gate high. In this event, the MOSFET's VGS is above the threshold voltage and begins to conduct, also pulling the higher voltage side down. As a result, both sides are pulled low. 
-
-[Note: ESP32 pin will sink 14.5V/560ohms = 26mA from 12V side, plus 3.3v/51kohm from 3.3v side.]
-
-### State 3 – 5V (12V) system pulls down the open-drain bus to a low level
-
-> With the higher voltage side pulling down on the bus line, the MOSFET's drain sits low. In this event, the MOSFET's drain-substrate diode begins to conduct, indirectly pulling the source low. When this happens, the MOSFET begins to conduct like in State 2 and, as a result, pulls down both sides of the bus line low. 
-
-[Note: Current draw 14.5v/560ohm = 26mA. (plus 3.3v/51kohm)]
-
-### Sources:
+To interface, I built a level-shifter using this forum post as a guide:
 
 https://forums.raspberrypi.com/viewtopic.php?t=119614
 
-Note: Author Jim G. uses two pins (R & W), 5V arduino, P-channel MOSFET. 
+Based on available components, and to make sure that the read pin sees no more than 3.3v, resistor values for voltage divider on read pin were changed to 10k & 3k.
+
+![My Image](SeeLevelSensorCircuit.png)
--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,23 @@
 
 Inspired by Jim G.: https://forums.raspberrypi.com/viewtopic.php?t=119614
 
-To read RV tanks equipped with the Garnet SeeLevel sensors, one can purchase 
+To read RV tanks equipped with the Garnet SeeLevel sensors, one can purchase
 a display monitor panel that has any of Bluetooth, RV-C, or NEMA2000 - all of
-which would be a better interface than this. This is an attempt to read the 
-sensors (senders) directly, without a Garnet display panel. 
+which would be a better interface than this. This is an attempt to read the
+sensors (senders) directly, without a Garnet display panel.
 
 Summary of information discovered by Jim G. and documented in the above post:
 
-  To trigger the tank sender/sensor, one needs to power the sender with 12V then 
-  pull the 12V line to ground in a particular pattern. The sender will respond 
-  by pulling the 12V line to ground with a series of pulses that can be 
+  To trigger the tank sender/sensor, one needs to power the sender with 12V then
+  pull the 12V line to ground in a particular pattern. The sender will respond
+  by pulling the 12V line to ground with a series of pulses that can be
   interpreted as bytes.
 
   Triggering the sender:
 
-  Each Garnet SeeLevel tank sender is configured as sensor 1, 2, or 3 by snipping 
+  Each Garnet SeeLevel tank sender is configured as sensor 1, 2, or 3 by snipping
   a tab on the sender. A sender will respond when it sees a sequence of pulses
-  to ground equal to its sensor number. Each pulse needs to be approx 85µs wide. 
+  to ground equal to its sensor number. Each pulse needs to be approx 85µs wide.
   Pulse spacing needs to be approximately 300µs.
 
   The sender will respond by pulling the 12V line to ground in a series of pulses.
@@ -34,7 +34,7 @@ Summary of information discovered by Jim G. and documented in the above post:
 
   For the segment fill values, a 'full' value will likely be less than 255, apparently
   depending on tank wall thickness, tank size, and perhaps how well the sender is
-  attached. In my testing, using a 710AR Rev E taped to a water jug, I see 'full' segments 
+  attached. In my testing, using a 710AR Rev E taped to a water jug, I see 'full' segments
   anywhere from 126 to 200. Pressing on a segment with my thumb will cause the sensor
   to read a higher value. Capacitance, perhaps?
 
@@ -44,21 +44,16 @@ Demo app - Output to Serial port:
 
 Interfacing 12V sensor with 3.3V ESP32:
 
-To interface, I built a level-shifter using this document as a guide:
+To interface, I wired a circuit using this document as a guide:
 
-  https://www.analog.com/en/design-notes/how-to-level-shift-1wire-systems.html
+  https://forums.raspberrypi.com/viewtopic.php?t=119614
 
-  The 'Basic Level Shifter' was built with the right-hand side powered by a 12V power
-  supply, with an ESP32 as the 3.3V side. Resisters were chosen by guessing. I ended 
-  up with a 51k pullup resister on the 3.3V side and a 560ohm resister on 
-  the 12V side. 
-
-  A cheap 12V-tolerant logic analyzer (LA1010) was used to assist 
-  in debugging.
+  Jim D.'s circuit appears to work fine. For an ESP32, the voltage divider
+  on the read pin should be modified to limit pin to no more than 3.3V
 
 Notes:
 
-  This app doesn't attempt to accommodate a trimmed sender or any sender other than the 
+  This app doesn't attempt to accommodate a trimmed sender or any sender other than the
   710AR Rev E.
 
   No attempt is made to process the returned data into an actual liquid level. I'm intending
@@ -67,15 +62,24 @@ Notes:
   I'm not confident in the validity of the checksum calculation.
 
   Uses Arduino framework but is only tested on an ESP32.
-  
-  When the ESP32 pin is pulled low, the ESP32 sinks current from both sides of the 
-  circuit, so care must be taken to prevent the ESP32 from going to smoke.
-  
+
 */
 
 #include <Arduino.h>
 
-int SeeLevelPIN = 13;  // ESP32 pin attached to level shifting circuit
+// ESP32 Write Pin. Set HIGH to power sensors
+const int SeeLevelWritePIN = 13;
+// ESP32 Read pin. Will be pulled low by sensors
+const int SeeLevelReadPIN = 16;
+
+// Time 12V bus is powered before sending pulse(s) to sensor(s)
+#define SEELEVEL_POWERON_DELAY 2450
+// Width of pulse sent to sensors
+#define SEELEVEL_PULSE_LOW_MICROSECONDS 85
+// Time between pulses snt to sensors
+#define SEELEVEL_PULSE_HIGH_MICROSECONDS 290
+// How long to wait for response before timing out
+#define SEELEVEL_PULSE_TIMEOUT 10000
 
 // Byte array to store data for 3 tanks x 12 bytes data per tank:
 byte SeeLevelData[3][12];
@@ -86,8 +90,9 @@ void readLevel(int);
 void setup() {
   Serial.begin(115200);
   Serial.println("\n\nTank Level is Woke!");
-  pinMode(SeeLevelPIN, OUTPUT);
-  digitalWrite(SeeLevelPIN, LOW);
+  pinMode(SeeLevelWritePIN, OUTPUT);
+  pinMode(SeeLevelReadPIN, INPUT);
+  digitalWrite(SeeLevelWritePIN, LOW);
   delay(5000);
 }
 
@@ -102,49 +107,58 @@ void loop() {
     }
 
     // Verify checksum
-    int byteSum = 0;                    
-    for (int i = 2; i <= 10; i++) {             // Sum data bytes 
+    int byteSum = 0;
+    for (int i = 2; i <= 10; i++) {  // Sum data bytes
       byteSum = byteSum + SeeLevelData[t][i];
     }
+    // Calculate and compare checksum
     int checkSum = SeeLevelData[t][1];
-    // Checksum appears to be (sum of bytes % 256) - 1, with special case for checksum 255. 
+    // Checksum appears to be (sum of bytes % 256) - 1, with special case for checksum 255.
     if (((byteSum % 256) - 1 == checkSum) || (byteSum % 256 == 0 && checkSum == 255)) {
-          Serial.print("Checksum: ");
-          Serial.print((byteSum % 256) - 1);
-          Serial.println(" OK");
+      Serial.print("Checksum: ");
+      Serial.print((byteSum % 256) - 1);
+      Serial.println(" OK");
     } else {
-      Serial.print(" byteSum % 256 - 1 = ");    
+      Serial.print(" byteSum % 256 - 1 = ");
       Serial.print(" Checksum: ");
       Serial.print((byteSum % 256) - 1);
       Serial.println(" Not OK");
     }
+    // Bus must be pulled low for some time before attempting to read another sensor
+    delay(1000);
   }
-  delay(5000);  // wait
+  delay(5000);  // wait between reads
 }
 
 //
 // Read an individual tank level
-// Pass parameter 't', where 
-//    t = 0 SeeLevel tank 1 (Normally Fresh Tank) 
+// Pass parameter 't', where
+//    t = 0 SeeLevel tank 1 (Normally Fresh Tank)
 //    t = 1 SeeLevel tank 2 (Normally Grey Tank)
 //    t = 2 SeeLevel tank 3 (Normally Black Tank)
 //
-void readLevel(int t) {               // Passed variable (t) is 0, 1 or 2
-  digitalWrite(SeeLevelPIN, HIGH);    // Power the sensor line for 2.4 ms so tank levels can be read
-  delayMicroseconds(2450);
-  for (int i = 0; i <= t; i++) {      // 1, 2 or 3 low pulses to select Fresh, Grey, Black tank
-    digitalWrite(SeeLevelPIN, LOW);
-    delayMicroseconds(85);            // Pulse 85µs down/290µs up
-    digitalWrite(SeeLevelPIN, HIGH);
-    delayMicroseconds(290);
+// Passed variable (t) is 0, 1 or 2
+//
+void readLevel(int t) {
+  
+  // Power the sensor line for 2.4 ms so tank levels can be read
+  digitalWrite(SeeLevelWritePIN, HIGH);
+  delayMicroseconds(SEELEVEL_POWERON_DELAY);
+
+  // 1, 2 or 3 low pulses to select Fresh, Grey, Black tank
+  for (int i = 0; i <= t; i++) {
+    digitalWrite(SeeLevelWritePIN, LOW);
+    delayMicroseconds(SEELEVEL_PULSE_LOW_MICROSECONDS);
+    digitalWrite(SeeLevelWritePIN, HIGH);
+    delayMicroseconds(SEELEVEL_PULSE_HIGH_MICROSECONDS);
   }
 
   // Read 12 bytes from sensor, store in array
   for (int byteLoop = 0; byteLoop < 12; byteLoop++) {
     SeeLevelData[t][byteLoop] = readByte();
   }
-  delay(10);                       
-  digitalWrite(SeeLevelPIN, LOW);      // Turn power off until next poll
+  delay(10);
+  digitalWrite(SeeLevelWritePIN, LOW);  // Turn power off until next poll
 }
 
 //
@@ -156,10 +170,10 @@ void readLevel(int t) {               // Passed variable (t) is 0, 1 or 2
 //
 // Each byte will roughly approximate the fill level of a segment of the sensor 0 <> 255
 //
-// A 'full' sensor segment will show somewhere between 126 and 255, depending on tank wall 
+// A 'full' sensor segment will show somewhere between 126 and 255, depending on tank wall
 // thickness, other factors.
 //
-// Testing can be done by temporarily attaching sensor to water jug or by simply touching 
+// Testing can be done by temporarily attaching sensor to water jug or by simply touching
 // sensor segments.
 //
 byte readByte() {
@@ -169,8 +183,8 @@ byte readByte() {
     result = result << 1;                           // Shift right for each incoming bit
 
     // Wait for low pulse, timeout if no pulse (I.E. no tank present)
-    unsigned long pulseTime = (pulseIn(SeeLevelPIN, LOW, 10000));
-      
+    unsigned long pulseTime = (pulseIn(SeeLevelReadPIN, LOW, SEELEVEL_PULSE_TIMEOUT));
+
     // Decide if pulse is logical '0', '1' or invalid
     if ((pulseTime >= 5) && (pulseTime <= 20)) {
       thisBit = 0;
@@ -180,7 +194,7 @@ byte readByte() {
       return 0;  // Pulse width out of range, Return zero
     }
     // Bitwise OR and shift pulses into single byte
-    result |= thisBit;  
+    result |= thisBit;
   }
   return result;
 }
